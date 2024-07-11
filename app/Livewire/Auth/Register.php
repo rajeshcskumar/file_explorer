c6953e95e4185abf06d62cbcde50464267e63e00
rajeshkumar1612169 <rajeshkumar1612169@gmail.com>
2024-07-12 01:01:59 +0500

    Register Functionilty

--- app/Livewire/Auth/Register.php
+++ app/Livewire/Auth/Register.php
@@ -2,12 +2,28 @@
 
 namespace App\Livewire\Auth;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Livewire\Component;
 
 class Register extends Component
 {
+    public $name, $email, $password, $password_confirmation;
     public function render()
     {
         return view('livewire.auth.register');
+    }
+    public function register()
+    {
+        $validate = $this->validate([
+            "name" => "required|min:2|max:30",
+            "email" => 'required|min:2|max:30|unique:users,email',
+            'password' => 'required|min:6|max:30|confirmed'
+        ]);
+
+        $validate["password"] = Hash::make($validate["password"]);
+        User::create($validate);
+        session()->flash('success', "Account created successfully!");
+        $this->redirect("/login", navigate: true);
     }
 }
--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -4,26 +4,38 @@
             <img src="{{asset('images/files.svg')}}" class="w-full object-contain" alt="auth_image">
         </div>
         <div class="h-screen flex justify-center items-center">
-            <form class="w-[500px]" action="">
+            <form class="w-[500px]" wire:submit="register">
                 <div>
                     <h1 class="text-2xl font-bold">Register</h1>
                     <p>Welcome to file explorer</p>
                 </div>
                 <div class="mb-2">
                     <label for="name">Name</label>
-                    <input type="text" class="input input-bordered w-full" id="name" name="name"   placeholder="Enter Name" />
+                    <input type="text" class="input input-bordered w-full" id="name" wire:model="name" placeholder="Enter Name" />
+                    @error('name')
+                        <span class="text-red-500">{{ $message }}</span>
+                    @enderror
                 </div>
                 <div class="mb-2">
                     <label for="email">Email</label>
-                    <input type="email" class="input input-bordered w-full" id="email" name="email"   placeholder="Enter Email" />
+                    <input type="email" class="input input-bordered w-full" id="email" wire:model="email" placeholder="Enter Email" />
+                    @error('email')
+                        <span class="text-red-500">{{ $message }}</span>
+                    @enderror
                 </div>
                 <div class="mb-2">
                     <label for="password">Password</label>
-                    <input type="password" class="input input-bordered w-full" id="password" name="password"   placeholder="Enter Password" />
+                    <input type="password" class="input input-bordered w-full" id="password" wire:model="password" placeholder="Enter Password" />
+                    @error('password')
+                        <span class="text-red-500">{{ $message }}</span>
+                    @enderror
                 </div>
                 <div class="mb-2">
                     <label for="password">Confirm Password</label>
-                    <input type="password" class="input input-bordered w-full" id="password" name="password"   placeholder="Enter Password" />
+                    <input type="password" class="input input-bordered w-full" id="password" wire:model="password_confirmation" placeholder="Enter Password" />
+                    @error('password_confirmation')
+                        <span class="text-red-500">{{ $message }}</span>
+                    @enderror
                 </div>
                 <div class="mb-2">
                     <button class="btn btn-warning w-full">Submit</button>
