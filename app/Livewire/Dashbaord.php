48115ae68bc182909f49d58d04f70c952bbdacb8
rajeshkumar1612169 <rajeshkumar1612169@gmail.com>
2024-07-14 03:51:33 +0500

    Show Image functionality

--- app/Livewire/Dashbaord.php
+++ app/Livewire/Dashbaord.php
@@ -2,12 +2,18 @@
 
 namespace App\Livewire;
 
+use App\Models\Files;
+use Auth;
 use Livewire\Component;
+use Livewire\Attributes\Title;
 
 class Dashbaord extends Component
 {
+    #[Title('dashbaord')]
     public function render()
     {
-        return view('livewire.dashbaord');
+        $user = Auth::guard('web')->user();
+        $files = Files::where('user_id', $user->id)->orderBy('id', 'desc')->get();
+        return view('livewire.dashbaord', ["files" => $files]);
     }
 }
--- resources/views/livewire/dashbaord.blade.php
+++ resources/views/livewire/dashbaord.blade.php
@@ -1,3 +1,26 @@
 <div>
     <x-common.toast />
+    <div class="px-2 flex justify-center flex-col items-center">
+        <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-2">
+            @if (count($files) > 0)
+                @foreach ($files as $item)
+                    <div class="card card-compact bg-base-100 w-96 shadow-xl">
+                        <figure>
+                            <img src="{{ asset('storage/' . $item->image) }}" alt="your_img"
+                                class="h-52 object-cover w-full" />
+                        </figure>
+                        <div class="card-body">
+                            <h2 class="card-title">{{ $item->title }}!</h2>
+                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                            <div class="card-actions justify-end">
+                                <button class="btn btn-primary">Buy Now</button>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            @else
+                <p>No Files Found</p>
+            @endif
+        </div>
+    </div>
 </div>
