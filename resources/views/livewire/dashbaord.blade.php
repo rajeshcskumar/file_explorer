3170be3fcf6d7137001751a5ed231aac60707098
rajeshkumar1612169 <rajeshkumar1612169@gmail.com>
2024-07-14 20:00:23 +0500

    Show Single File

--- resources/views/livewire/dashbaord.blade.php
+++ resources/views/livewire/dashbaord.blade.php
@@ -1,14 +1,15 @@
 <div>
     <x-common.toast />
-    <div class="px-2 lg:px-32 flex justify-center flex-col items-center">
+    <div class="px-2 flex justify-center flex-col items-center">
         <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-2">
             @if (count($files) > 0)
                 @foreach ($files as $item)
                     <div class="card card-compact bg-base-100 w-96 shadow-xl">
-                        <figure>
-                            <img src="{{ asset('storage/' . $item->image) }}" alt="your_img"
-                                class="h-52 object-cover w-full" />
-                        </figure>
+                        <a href="{{ route('show.file', ['id' => $item->id]) }}" wire:navigate>
+                            <figure><img src="{{ asset('storage/' . $item->image) }}" alt="your_img"
+                                    class="h-52 object-cover w-full" />
+                            </figure>
+                        </a>
                         <div class="card-body">
                             <h2 class="card-title">{{ $item->title }}!</h2>
                             <p>{{ $item->created_at->format('d-m-Y h:m:s a') }}</p>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Middleware\Authenticate;
 use App\Livewire\Auth\Login;
 use App\Livewire\Auth\Register;
 use App\Livewire\Dashbaord;
+use App\Livewire\Files\ShowFile;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
@@ -14,4 +15,5 @@ Route::middleware('guest')->group(function () {
 //Route::middleware('auth')->group(function () {
 Route::middleware([Authenticate::class])->group(function () {
     Route::get('/', Dashbaord::class)->name('dashboard');
+    Route::get("/file/{id}", ShowFile::class)->name("show.file")->lazy();
 });
